1fc20d9ff7be933adc01c3ff436818723aa4f792
rclark72 <rclark72@d68364d2-591d-d464-8a7f-fb39578533eb>
2010-05-03 17:40:54 +0000

    prelim segmented figure working

--- trunk/ArticulatedFigure/ArticulatedFigure.cpp
+++ trunk/ArticulatedFigure/ArticulatedFigure.cpp
@@ -7,9 +7,84 @@
 #define WIDTH 500
 #define HEIGHT 500
 
-void SetPixel(int x, int y)
+#define MAX_JCOUNT 6
+
+typedef struct joint_t
 {
-	glRecti(x, y, x+1, y+1);
+	float x, y, z, angle1, angle2; // Start
+	int childCount;
+	struct joint_t *children[MAX_JCOUNT], *parent;
+} Joint;
+
+Joint *addJoint(Joint *parent, float x, float y, float z, float angle1, float angle2)
+{
+	Joint *newJoint;
+	if(!parent)
+	{
+		parent = (Joint *)malloc(sizeof(Joint));
+		parent->parent = NULL;
+	}
+	else if(parent->childCount < MAX_JCOUNT)
+	{
+		newJoint = (Joint *)malloc(sizeof(Joint));
+		newJoint->parent = parent;
+		parent->children[parent->childCount++] = newJoint;
+		parent = newJoint;
+	}
+
+	parent->x = x;
+	parent->y = y;
+	parent->z = z;
+	parent->angle1 = angle1;
+	parent->angle2 = angle2;
+	parent->childCount = 0;
+
+	for(int x = 0; x < MAX_JCOUNT; x++)
+		parent->children[x] = NULL;
+
+	return parent;
+}
+
+Joint *root;
+void SetupFigure(void)
+{
+	root = addJoint(NULL, 0, 0, 0, 0, 0);
+	Joint *waist = addJoint(root, 0, -0.1, 0, 0, 0);
+	Joint *knee1 = addJoint(waist, -0.02, -0.05, 0, 0, 0);
+	Joint *knee2 = addJoint(waist, 0.02, -0.05, 0, 0, 0);
+	Joint *foot1 = addJoint(knee1, 0, -0.05, 0, 0, 0);
+	Joint *foot2 = addJoint(knee2, 0, -0.05, 0, 0, 0);
+	Joint *elbow1 = addJoint(root, -0.02, -0.05, 0, 0, 0);
+	Joint *elbow2 = addJoint(root, 0.02, -0.05, 0, 0, 0);
+	Joint *hand1 = addJoint(elbow1, 0, 0, 0.02, 0, 0);
+	Joint *hand2 = addJoint(elbow2, 0, 0, 0.02, 0, 0);
+}
+void DrawJoint(Joint *node)
+{
+	glPushMatrix();
+
+	glBegin(GL_LINES);
+		glColor3f(1, 0, 0);
+		glVertex3f(0, 0, 0);
+		glColor3f(0, 1, 0);
+		glVertex3f(node->x, node->y, node->z);
+	glEnd();
+
+	glTranslatef(node->x, node->y, node->z);
+	for(int i = 0; i < node->childCount; i++)
+	{
+		DrawJoint(node->children[i]);
+	}
+	glPopMatrix();
+	return;
+}
+
+void DrawFigure(float x, float y, float z)
+{
+	glPushMatrix();
+	glTranslatef(x, y, z);
+	DrawJoint(root);
+	glPopMatrix();
 }
 
 void DrawStage(void)
@@ -43,8 +118,7 @@ void DrawAxis(void)
 		glVertex3f(0, 0, 0.1);
 	glEnd();
 }
-struct coord_t { float x; float y; float z; } ;
-typedef coord_t Coord;
+typedef struct coord_t { float x; float y; float z; } Coord;
 
 Coord eye = { 1.5, 1, 1.5 };
 Coord lookat = { 0.5, 0, 0.5 };
@@ -60,6 +134,7 @@ void display(void)
 	glPushMatrix();
 		DrawStage();
 		DrawAxis();
+		DrawFigure(0.5, 0.2, 0.5);
 	glPopMatrix();
 	glutSwapBuffers();
 }
@@ -78,7 +153,8 @@ int main(int argc, char** argv)
 	gluPerspective(60.0, 1.0, 1.0, 10.0);
 	gluLookAt( eye.x, eye.y, eye.z, lookat.x, lookat.y, lookat.z, top.x, top.y, top.z );
 
-	DrawStage();
+	SetupFigure();
+	//DrawStage();
 	glClearColor (0.0, 0.0, 0.0, 0.0);
 	glutDisplayFunc(display); 
 	glutMainLoop();
