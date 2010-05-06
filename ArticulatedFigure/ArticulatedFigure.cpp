c1034616c1fdcc599b31658b9a91eac2680b885e
rclark72 <rclark72@d68364d2-591d-d464-8a7f-fb39578533eb>
2010-05-06 07:13:52 +0000

    Animation works

--- ArticulatedFigure/ArticulatedFigure.cpp
+++ ArticulatedFigure/ArticulatedFigure.cpp
@@ -1,70 +1,83 @@
 #include <stdlib.h>
 #include <time.h>
 #include <GL/glut.h>
-#include <string.h>
+#include <string>
 #include <stdio.h>
 #include <vector>
 #define WIDTH 500
 #define HEIGHT 500
-
+#define FIGURE "human.txt"
+#define ANIMATION "animation.txt"
 #define MAX_JCOUNT 6
 #define MAX_KFCOUNT 512
 
 bool typing = false;
+bool keyframe_typing = false;
 int cmd_buffer_len = 0;
 char cmd_buffer[100];
 int frameNum = 0;
 
+using namespace std;
+
 //Define a keyframe for joints
-typedef struct
+typedef struct keyframe_t
 {
 	int time;
 	float angle1;
 	float angle2;
+	struct keyframe_t *next;
 } Keyframe;
 
 typedef struct joint_t
 {
-        char name[100];
-        float x, y, z, angle1, angle2, offA1, offA2; // Start
-        int childCount;
-        struct joint_t *children[MAX_JCOUNT], *parent;
+    char name[100];
+    float x, y, z, angle1, angle2, offA1, offA2; // Start
+    int childCount;
+    struct joint_t *children[MAX_JCOUNT], *parent;
 	int keyframeCount;
-	Keyframe keyframe[MAX_KFCOUNT];
+	Keyframe *keyframe;
+	Keyframe *curframe;
 } Joint;
 
 
 /* This adds a joint to a parent, or initializes it as the root if there are no parents */
 Joint *addJoint(Joint *parent, float x, float y, float z, float angle1, float angle2, char name[])
 {
-        Joint *newJoint;
-        if(!parent)
-        {
-                parent = (Joint *)malloc(sizeof(Joint));
-                parent->parent = NULL;
-        }
-        else if(parent->childCount < MAX_JCOUNT)
-        {
-                newJoint = (Joint *)malloc(sizeof(Joint));
-                newJoint->parent = parent;
-                parent->children[parent->childCount++] = newJoint;
-                parent = newJoint;
-        }
+	Joint *newJoint;
+    if(!parent)
+    {
+        parent = (Joint *)malloc(sizeof(Joint));
+        parent->parent = NULL;
+    }
+    else if(parent->childCount < MAX_JCOUNT)
+    {
+        newJoint = (Joint *)malloc(sizeof(Joint));
+        newJoint->parent = parent;
+        parent->children[parent->childCount++] = newJoint;
+        parent = newJoint;
+    }
 
-        parent->x = x;
-        parent->y = y;
-        parent->z = z;
-        parent->angle1 = angle1;
-        parent->angle2 = angle2;
-	//parent->offA1 = 0;
-	//parent->offA2 = 0;
-        parent->childCount = 0;
-        strcpy(parent->name, name);
+    parent->x = x;
+    parent->y = y;
+    parent->z = z;
+    parent->angle1 = angle1;
+    parent->angle2 = angle2;
 
-        for(int x = 0; x < MAX_JCOUNT; x++)
-                parent->children[x] = NULL;
+	Keyframe *newframe = (Keyframe *)malloc(sizeof(Keyframe));
+	newframe->angle1 = angle1;
+	newframe->angle2 = angle2;
+	newframe->time = 0;
+	newframe->next = NULL;
+	parent->curframe = newframe;
+	parent->keyframe = newframe;
 
-        return parent;
+    parent->childCount = 0;
+    strcpy(parent->name, name);
+
+    for(int x = 0; x < MAX_JCOUNT; x++)
+        parent->children[x] = NULL;
+
+    return parent;
 }
 
 Joint *root;
@@ -72,198 +85,213 @@ Joint *selected;
 
 Joint *FindJoint(Joint *node, char name[])
 {
-        if(strcmp(node->name, name) == 0)
-                return node;
-        else
+    if(strcmp(node->name, name) == 0)
+        return node;
+    else
+    {
+        Joint *found = NULL;
+        for(int i = 0; i < node->childCount; i++)
         {
-                Joint *found = NULL;
-                for(int i = 0; i < node->childCount; i++)
-                {
-                        found = FindJoint(node->children[i], name);
-                        if(found)
-                                return found;
-                }
+            found = FindJoint(node->children[i], name);
+            if(found)
+                return found;
         }
-        return NULL;
+    }
+    return NULL;
 
+}
+
+Keyframe *addKeyframe(Joint *joint, int time, float angle1, float angle2)
+{
+	Keyframe *newframe = (Keyframe *)malloc(sizeof(Keyframe));
+	newframe->time = time;
+	newframe->angle1 = angle1;
+	newframe->angle2 = angle2;
+	newframe->next = NULL;
+	Keyframe *oldframe = joint->keyframe;
+	while(oldframe->next != NULL)
+		oldframe = oldframe->next;
+	oldframe->next = newframe;
+	if(!joint->curframe)
+		joint->curframe = newframe;
+	return newframe;
 }
 
 void LoadFigure(char *path)
 {
-        // parent x y z angle1 angle2 name
-        FILE *file;
-        float x=0, y=0, z=0, angle1=0, angle2=0;
+    // parent x y z angle1 angle2 name
+    FILE *file;
+    float x=0, y=0, z=0, angle1=0, angle2=0;
 	int time;
-        char name[20], parent[20], buffer[512], animBuf[512], *ptr, *token;
-        Joint *tmpJoint;
+    char name[20], parent[20], buffer[512];
+    Joint *tmpJoint;
 	Keyframe *k;
 
-        if(!(file = fopen(path, "r")))
+    if(!(file = fopen(path, "r")))
+    {
+        printf("Unable to load file\n");
+        return;
+    }
+
+    while(!feof(file))
+    {
+        fgets(buffer, 512, file);
+        sscanf(buffer, "%s %f %f %f %f %f %s\n", parent, &x, &y, &z, &angle1, &angle2, name);
+        if(strcmp(parent, "0") == 0)
+            root = addJoint(NULL, x, y, z, angle1, angle2, name);
+        else
         {
-                printf("Unable to load file\n");
-                return;
+            tmpJoint = FindJoint(root, parent);
+            addJoint(tmpJoint, x, y, z, angle1, angle2, name);
         }
-
-        while(!feof(file))
-        {
- 		memset(animBuf, 0, 1024);
-                fgets(buffer, 512, file);
-                sscanf(buffer, "%s %f %f %f %f %f %s %[^\n]", parent, &x, &y, &z, &angle1, &angle2, name, animBuf);
-                if(strcmp(parent, "0") == 0)
-                        root = addJoint(NULL, x, y, z, angle1, angle2, name);
-                else
-                {
-                        tmpJoint = FindJoint(root, parent);
-                        addJoint(tmpJoint, x, y, z, angle1, angle2, name);
-                }
-
-		/* Now check for animation data and set up keyframes */
-		if (strlen(animBuf) > 3)
-		{
-			ptr = animBuf;
-			while ((token = strtok(ptr, " ")))
-			{
-				ptr = NULL;
-				time = atoi(token);
-			
-				token = strtok(ptr, " ");
-				angle1 = atof(token);
-
-				token = strtok(ptr, " ");
-				angle2 = atof(token);
-
-				//printf("Read %d %f\n", time, angle1);
-
-				if (tmpJoint->keyframeCount >= MAX_KFCOUNT)
-				{
-					printf("Can't add more keyframes\n");
-					continue;
-				}
-				
-				k = &(tmpJoint->keyframe[tmpJoint->keyframeCount]);
-
-				k->time = time;
-				k->angle1 = angle1;
-				k->angle2 = angle2;
-			
-				tmpJoint->keyframeCount++;
-			}
-		}
 	}
 	selected = root;
-        fclose(file);
+    fclose(file);
+}
+
+void LoadAnimation(char *path)
+{
+	FILE *file;
+	char buffer[512], name[20];
+	float angle1, angle2;
+	int time;
+
+    if(!(file = fopen(path, "r")))
+    {
+        printf("Unable to load file\n");
+        return;
+    }
+	
+	while(!feof(file))
+    {
+        fgets(buffer, 512, file);
+		name[0] = 0;
+        if(sscanf(buffer, "%s %f %f %d\n", name, &angle1, &angle2, &time) == 4)
+		{
+			Joint *framejoint = FindJoint(root, name);
+			addKeyframe(framejoint, time, angle1, angle2);
+		}
+
+	}
+
+    fclose(file);
+}
+
+void SaveTraversal(Joint *node, int time, char* buffer)
+{
+	printf("%f %f %d\n", node->angle1, node->angle2, time);
+	char tmpbuffer[128];
+	sprintf(buffer+strlen(buffer), "%s %f %f %d\n", node->name, node->angle1, node->angle2, time);
+	//strcat(buffer, tmpbuffer);
+	for(int i = 0; i < node->childCount; i++)
+    {
+        SaveTraversal(node->children[i], time, buffer);
+    }
+}
+
+void SaveKeyframe(char *path, int time)
+{
+	FILE *file;
+	char buffer[1024] = "";
+    if(!(file = fopen(path, "a")))
+    {
+        printf("Unable to load file\n");
+        return;
+    }
+	SaveTraversal(root, time, buffer);
+	fprintf(file, buffer);
+	fclose(file);
+}
+
+//Animate the joint in question using interpolation
+void jointAnimate(Joint *joint, int time)
+{
+	if(joint->curframe != NULL && joint->curframe->next != NULL && joint->curframe->next->time < time)
+		joint->curframe = joint->curframe->next;
+	if(joint->curframe != NULL && joint->curframe->next != NULL)
+	{
+		int cur_time = time - joint->curframe->time;
+		int tdelta = joint->curframe->next->time - joint->curframe->time;
+		float fraction = (float)cur_time/tdelta;
+
+		joint->angle1 = joint->curframe->angle1 + (cur_time)*(joint->curframe->next->angle1 - joint->curframe->angle1)/tdelta;
+		printf("angle1: %f %f %f\n", joint->angle1, joint->curframe->angle1, joint->curframe->next->angle1);
+		joint->angle2 = joint->curframe->angle2 + (cur_time)*(joint->curframe->next->angle2 - joint->curframe->angle2)/tdelta;
+	}
+	
 }
 
 // Draws a joint and traverses to its children to draw those
 void DrawJoint(Joint *node)
 {
-        glPushMatrix();
-        glRotatef(node->angle1, 0.0, 0.0, 1.0);
-        glRotatef(node->angle2, 1.0, 0.0, 0.0);
-        glBegin(GL_LINES);
-                if(node == selected)
-                        glColor3f(0, 1, 0);
-                else
-                        glColor3f(1, 0, 0);
-                glVertex3f(0, 0, 0);
-                glVertex3f(node->x, node->y, node->z);
-        glEnd();
+	int time = glutGet(GLUT_ELAPSED_TIME);
+	jointAnimate(node, time);
 
-        glTranslatef(node->x, node->y, node->z);
-        for(int i = 0; i < node->childCount; i++)
-        {
-                DrawJoint(node->children[i]);
-        }
-        glPopMatrix();
-        return;
+    glPushMatrix();
+    glRotatef(node->angle1, 0.0, 0.0, 1.0);
+    glRotatef(node->angle2, 1.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+        if(node == selected)
+            glColor3f(0, 1, 0);
+        else
+            glColor3f(1, 0, 0);
+        glVertex3f(0, 0, 0);
+        glVertex3f(node->x, node->y, node->z);
+    glEnd();
+
+    glTranslatef(node->x, node->y, node->z);
+    for(int i = 0; i < node->childCount; i++)
+    {
+        DrawJoint(node->children[i]);
+    }
+    glPopMatrix();
+    return;
 }
-
-
-
-//Animate the joint in question using interpolation
-void jointAnimate(Joint *root, int time)
- {
- 	int i;
- 
- 	float ang1,ang2,tim;
- 
- 	/* Check for keyframes */
- 	for (i = 0; i < root->keyframeCount; i++)			
- 		if (root->keyframe[i].time == time)
- 		{
- 			/* Find the index for the interpolation */
- 			if (i != root->keyframeCount - 1)
- 			{
- 				tim = root->keyframe[i + 1].time - root->keyframe[i].time;
- 				ang1 = root->keyframe[i + 1].angle1 - root->keyframe[i].angle1;
- 				ang2 = root->keyframe[i + 1].angle2 - root->keyframe[i].angle2;
- 
- 				root->offA1 = ang1 / tim;
- 				root->offA2 = ang2 / tim;
-  			}
- 			else
- 			{
- 				root->offA1 = 0;
- 				root->offA2 = 0;
- 			}
- 		}
- 
- 	/* Change animation */
- 	root->angle1 += root->offA1;
- 	root->angle2 += root->offA2;
-
-	DrawJoint(root); 
-
- 	/* Call on other bones */
- 	for (i = 0; i < root->childCount; i++)
- 		jointAnimate(root->children[i], time);
-	
- }
-
 
 // Draws the figure
 void DrawFigure(float x, float y, float z)
 {
 	int i;
-        glPushMatrix();
-        glTranslatef(x, y, z);
+    glPushMatrix();
+    glTranslatef(x, y, z);
+	DrawJoint(root);
 	//for(i = 0; i < 10; i++)
-        	jointAnimate(root , 10 );
-        glPopMatrix();
+    //jointAnimate(root , 10 );
+    glPopMatrix();
 }
 
 // Draws the stage that the figure will be located on
 void DrawStage(void)
 {
-        glBegin(GL_POLYGON);
-                glColor3f(0.3, 0.3, 0.3);
-                glVertex3f(0, 0, 0);
-                glVertex3f(0, 0, 1);
-                glVertex3f(1, 0, 1);
-                glVertex3f(1, 0, 0);
-        glEnd();
+    glBegin(GL_POLYGON);
+        glColor3f(0.3, 0.3, 0.3);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 0, 1);
+        glVertex3f(1, 0, 1);
+        glVertex3f(1, 0, 0);
+    glEnd();
 }
 
 // Draws the xyz axis for reference
 void DrawAxis(void)
 {
-        glBegin(GL_LINES);
-                glColor3f(1, 0, 0);
-                glVertex3f(0, 0, 0);
-                glVertex3f(0.1, 0, 0);
-        glEnd();
+    glBegin(GL_LINES);
+        glColor3f(1, 0, 0);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0.1, 0, 0);
+    glEnd();
 
-        glBegin(GL_LINES);
-                glColor3f(0, 1, 0);
-                glVertex3f(0, 0, 0);
-                glVertex3f(0, 0.1, 0);
-        glEnd();
+    glBegin(GL_LINES);
+        glColor3f(0, 1, 0);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 0.1, 0);
+    glEnd();
 
-        glBegin(GL_LINES);
-                glColor3f(0, 0, 1);
-                glVertex3f(0, 0, 0);
-                glVertex3f(0, 0, 0.1);
-        glEnd();
+    glBegin(GL_LINES);
+        glColor3f(0, 0, 1);
+        glVertex3f(0, 0, 0);
+        glVertex3f(0, 0, 0.1);
+    glEnd();
 }
 
 // Displays a command window to select figure joints
@@ -275,90 +303,159 @@ void DisplayCmd() {
     glMatrixMode(GL_MODELVIEW);
     glPushMatrix();
 
-        glColor3f(1.0, 1.0, 1.0);
-        glRasterPos2f( 0.5, 0.8 );
-        for(int i = 0; i < cmd_buffer_len; i++) {
-                glutBitmapCharacter( GLUT_BITMAP_TIMES_ROMAN_24, cmd_buffer[i]);
-        }
-        glutBitmapCharacter( GLUT_BITMAP_TIMES_ROMAN_24, '_');
+    glColor3f(1.0, 1.0, 1.0);
+    glRasterPos2f( 0.5, 0.8 );
+    for(int i = 0; i < cmd_buffer_len; i++) {
+        glutBitmapCharacter( GLUT_BITMAP_TIMES_ROMAN_24, cmd_buffer[i]);
+    }
+    glutBitmapCharacter( GLUT_BITMAP_TIMES_ROMAN_24, '_');
 
-        glMatrixMode(GL_PROJECTION);                        // Select The Projection Matrix
+    glMatrixMode(GL_PROJECTION);                        // Select The Projection Matrix
     glPopMatrix();                                      // Restore The Old Projection Matrix
     glMatrixMode(GL_MODELVIEW);                         // Select The Modelview Matrix
     glPopMatrix();                                      // Restore The Old Projection Matrix
 }
-typedef struct coord_t { float x; float y; float z; } Coord;
-
-Coord eye = { 1.5, 0.5, 1.5 };
-Coord lookat = { 0.5, 0, 0.5 };
-Coord top = { 0, 1, 0 };
 
 void display(void)
 {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glColor3f(1,1,1);
-        glPushMatrix();
-                DrawStage();
-                DrawAxis();
-                DrawFigure(0.8, 0.2, 0.6);
-                if(typing)
-                        DisplayCmd();
-        glPopMatrix();
-        glutSwapBuffers();
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1,1,1);
+    glPushMatrix();
+        DrawStage();
+        DrawAxis();
+        DrawFigure(0.8, 0.2, 0.6);
+        if(typing)
+            DisplayCmd();
+    glPopMatrix();
+    glutSwapBuffers();
 }
 
+typedef struct coord_t { float x; float y; float z; } Coord;
+Coord eye = { 1.5, 0.5, 1.5 };
+Coord lookat = { 0.5, 0, 0.5 };
+Coord top = { 0, 1, 0 };
+void rotateScene(float angle1, float angle2, float angle3)
+{
+	glTranslatef(0.5, 0, 0.5);
+	glRotatef(angle3, 0, 0, 1);
+	glRotatef(angle2, 1, 0, 0);
+	glRotatef(angle1, 0, 1, 0);
+	//gluLookAt( 0, 0, 0, lookat.x, lookat.y, lookat.z, top.x, top.y, top.z );
+	glTranslatef(-0.5, 0, -0.5);
+	glutPostRedisplay();
+	//gluLookAt( eye.x, eye.y, eye.z, lookat.x, lookat.y, lookat.z, top.x, top.y, top.z );
+	return;
+}
 
 void motion(int x, int y)
 {
-        selected->angle1 = x;
-        selected->angle2 = y;
-        display();
+    selected->angle1 = x;
+    selected->angle2 = y;
+	glutPostRedisplay();
+}
+
+void idle(void)
+{
+	glutPostRedisplay();
 }
 
 void keyboard(unsigned char key, int x, int y)
 {
-        if(key == 'c' && typing == false)
-                typing = true;
-        else if (typing == true && key == 27) {
-                cmd_buffer[0] = 0;
-                cmd_buffer_len = 0;
-                typing = false;
-        } else if(typing == true && key == 8)
-                cmd_buffer[cmd_buffer_len--] = 0;
-        else if(typing == true && key == 13) {
-                selected = FindJoint(root, cmd_buffer);
-                cmd_buffer[0] = 0;
-                cmd_buffer_len = 0;
-                typing = false;
-        }
-        else if(typing == true)
-        {
-                cmd_buffer[cmd_buffer_len++] = key;
-                cmd_buffer[cmd_buffer_len] = 0;
-        }
-        display();
+    if(typing == true) 
+	{
+		if(key == 27)
+		{
+			// ESC key
+			cmd_buffer[0] = 0;
+			cmd_buffer_len = 0;
+			typing = false;
+			keyframe_typing = false;
+		}
+		else if(key == 8 && cmd_buffer_len > 0)
+		{
+			// Backspace
+			cmd_buffer[cmd_buffer_len--] = 0;
+		}
+		else if(key == 13)
+		{
+			// Enter key
+			if(keyframe_typing)
+				SaveKeyframe(ANIMATION, atoi(cmd_buffer));
+			else
+				selected = FindJoint(root, cmd_buffer);
+			cmd_buffer[0] = 0;
+			cmd_buffer_len = 0;
+			typing = false;
+			keyframe_typing = false;
+		}
+		else
+		{
+			// Any other key
+			cmd_buffer[cmd_buffer_len++] = key;
+			cmd_buffer[cmd_buffer_len] = 0;
+		}
+	}
+	else if(key == 'j')
+		rotateScene(-0.3, 0, 0);
+	else if(key =='l')
+		rotateScene(0.3, 0, 0);
+	else if(key == 'u')
+		rotateScene(0, -0.3, 0);
+	else if(key == 'o')
+		rotateScene(0, 0.3, 0);
+	else if(key == '7')
+		rotateScene(0, 0, -0.3);
+	else if(key == '9')
+		rotateScene(0, 0, 0.3);
+	else if(key == 'r')
+		LoadFigure(FIGURE);
+	else
+	{
+		// Enter command mode
+		if(key == 'c')
+			typing = true;
+		else if(key == 'v')
+		{
+			typing = true;
+			keyframe_typing = true;
+		}
+		else if(selected != NULL)
+		{
+			if(key == 'w')
+				selected->angle1 += .1;
+			else if(key == 's')
+				selected->angle1 -= .1;
+			else if(key == 'd')
+				selected->angle2 += .1;
+			else if(key == 'a')
+				selected->angle2 -= .1;
+		}
+	}
+	glutPostRedisplay();
 }
 int main(int argc, char** argv)
 {
-        glutInit(&argc, argv);
-        glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-        glutInitWindowSize (WIDTH, HEIGHT);
-        glutInitWindowPosition (500, 100);
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize (WIDTH, HEIGHT);
+	glutInitWindowPosition (500, 100);
 
-        glutCreateWindow (argv[0]);
+	glutCreateWindow (argv[0]);
 
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        gluPerspective(60.0, 1.0, 1.0, 10.0);
-        gluLookAt( eye.x, eye.y, eye.z, lookat.x, lookat.y, lookat.z, top.x, top.y, top.z );
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluPerspective(60.0, 1.0, 1.0, 10.0);
+	gluLookAt( eye.x, eye.y, eye.z, lookat.x, lookat.y, lookat.z, top.x, top.y, top.z );
 
-        LoadFigure("wave.txt");
-        glClearColor (0.0, 0.0, 0.0, 0.0);
-        glutDisplayFunc(display);
-       // glutMotionFunc(motion);
-        glutKeyboardFunc(keyboard);
-        glutMainLoop();
+	LoadFigure(FIGURE);
+	LoadAnimation(ANIMATION);
+	glClearColor (0.0, 0.0, 0.0, 0.0);
+	glutDisplayFunc(display);
+	// glutMotionFunc(motion);
+	glutKeyboardFunc(keyboard);
+	glutIdleFunc(idle);
+	glutMainLoop();
 
-        return 0;
+	return 0;
 }
 
